4314f5bc5231eddecc1c3e412752b6b4dbfb3d40
Robert Booth <robert.booth@trebortech.com>
2016-04-14 11:41:32 -0500

    New app control module

---
+++ _modules/appctl.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+'''
+Module to help with application deployments
+
+'''
+
+
+from __future__ import absolute_import
+
+
+# Import salt libs
+import salt.utils
+
+# Define the module's virtual name
+__virtualname__ = 'appctl'
+
+
+def __virtual__():
+    return __virtualname__
+
+
+def deploy(appname, version):
+
+    ret = __salt__['state.sls'](
+        mods='sites.{0}'.format(appname),
+        pillar='{"version":"{0}"}'.format(version)
+        )
+
+    return ret
