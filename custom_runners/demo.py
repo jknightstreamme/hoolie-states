666009dfb5d319797fec3c0989804d02055c7ca4
Robert Booth <robert.booth@trebortech.com>
2016-04-15 17:47:23 -0500

    new demo runner

---
+++ custom_runners/demo.py
@@ -0,0 +1,32 @@
+'''
+Runner for demo code
+'''
+
+from __future__ import absolute_import
+
+import logging
+import salt.runners.lambda
+import salt.client
+
+
+def __virtual__():
+    return 'demo'
+
+
+def giphyget(keyword):
+
+    # get giphy
+    ret = salt.runners.lambda.giphyget(keyword)
+
+    # update web pages
+
+    fun = 'state.sls'
+    tgt = 'saltconf:True'
+    expr_form = 'grain'
+
+    args = ('updatewebpage')
+    kwarg = {"pillar": ret}
+    local = salt.client.get_local_client(__opts__['conf_file'])
+    cmdret = local.cmd(tgt, expr_form, fun, args, kwarg)
+
+    return True
