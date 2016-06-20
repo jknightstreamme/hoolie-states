bc588732c8b9265d2b60ca4d595b7c5d264e9d3f
Robert Booth <robert.booth@trebortech.com>
2016-06-20 17:57:19 -0500

    Testing of custom state for checks

---
+++ _states/checks.py
@@ -0,0 +1,44 @@
+'''
+Updates from events
+
+'''
+import logging
+
+# Import salt libs
+import salt.utils
+
+log = logging.getLogger(__name__)
+
+try:
+    import RPi.GPIO as GPIO
+    import pyttsx
+    HAS_LIBS = True
+except ImportError:
+    HAS_LIBS = False
+
+# Define the module's virtual name
+__virtualname__ = 'checks'
+
+
+def __virtual__():
+    if HAS_LIBS:
+        return __virtualname__
+
+
+def http(name, status):
+
+    ret = {'name': name,
+           'changes': {},
+           'result': True,
+           'comment': ''}
+
+    kwargs['status'] = True
+    #data = __salt__['http.query'](name, **kwargs)
+
+    #if data.get('status', '') == status:
+    __salt__['mine.send']('http.query', name=name, **kwargs)
+
+    
+    ret['comment'] = 'http status updated'
+
+    return ret
